6b746f3988c03dc53b5a2409eb559e06e5c8e378
DatScribble <33287563+DatScribble@users.noreply.github.com>
2020-09-09 17:06:41 -0400

    Finished ClackData, working on MessageClackData

--- data/src/main/java/MessageClackData.java
+++ data/src/main/java/MessageClackData.java
@@ -1,14 +1,33 @@
 public class MessageClackData extends ClackData  {
 
-    String message;
+    private String message;
 
     public MessageClackData(String userName, String messaage, int type){
         super(userName, type);
-        String message;
+        this.message = message;
+    }
+
+    public MessageClackData(){
+        super();
+        this.message = "";
+    }
+    @Override
+    public String getData() {
+        return message;
     }
 
     @Override
-    public String getData() {
-        return null;
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
     }
 }
